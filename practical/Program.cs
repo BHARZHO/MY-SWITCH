bab7fd69a41e21ef96db92529965774c5eb6ba2e
Abdulsalam Abdulbasit <abdulbasitabdulsalam@gmail.com>
2023-09-21 02:25:53 -0700

    my first switch statement

---
+++ practical/Program.cs
@@ -0,0 +1,82 @@
+﻿/*Console.WriteLine("Welcome to MGQS: Input your gender");
+string gender = Console.ReadLine()!;
+gender = gender.ToLower();
+
+switch(gender)
+{
+    case "male":
+       Console.WriteLine("Asslamualikum warahmatullahi wabarakaatuhu ,you are a male gender");
+    break;
+
+    case "female":
+       Console.WriteLine("Asslamualikum warahmatullahi wabarakaatuhu , you are a female gender");
+    break;
+
+    default:
+       Console.WriteLine("Asslamualikum warahmatullahi waabarakaatuhu you no dey transparent");
+    break;
+}*/
+
+
+//MTN suscription App
+
+/*Console.WriteLine("MTN data bundle suscription reminder: Awuff");
+string getMessage(int daysRemaining);
+string message;
+switch(daysRemaining)
+{
+   case "Five days reminder message":
+      Console.WriteLine("Your data bundle will expire in five days time buh if you roll over before then you will be given 20% discount");
+   break;
+
+   case "Four days reminder message":
+      Console.WriteLine("Your data bundle will expire in four days time , buh if you roll over before then you will be given 15% discount");
+   break;
+
+   case "One day remainder message":
+      Console.WriteLine("Your data bundle will expire in a day time, buh if you roll over brfore then you will be given 10% discount");
+   break;
+
+
+    
+}*/
+
+
+// Console.WriteLine("Calculator \n What operation would you like to calculate");
+// string operation = Console.ReadLine()!.ToLower();
+// Console.WriteLine("Enter two numbers you wanna calculate"); 
+// Console.WriteLine("-------------------------------------");
+// Console.WriteLine("Enter First Number");
+// int number1 = int.Parse(Console.ReadLine()!); 
+// Console.WriteLine("Enter The Second Number");
+// int number2 = int.Parse(Console.ReadLine()!);
+// int operationResult;
+// switch(operation)
+// {
+//    case "+":
+//      operationResult = number1 + number2;
+//      Console.WriteLine($"Your Answer is:{operationResult}");
+//    break;
+
+//    case "-":
+//      operationResult = number1 - number2;
+//      Console.WriteLine($"Your Answer is:{operationResult}");
+//    break;
+
+//    case "*":
+//      operationResult = number1 * number2;
+//      Console.WriteLine($"Your Answer is:{operationResult}");
+//    break;
+
+//    case "/":
+//      operationResult = number1 / number2;
+//      Console.WriteLine($"Your Answer is:{operationResult}");
+//    break;
+
+//    default:
+//      Console.WriteLine("You dont know what you wanna calculate besides my calculator can't calculate that yet");
+//    break;
+
+// }
+
+ 
